6e2fcb18421e36628dc5fd622b036ec2a81d236e
Developer User <developer@h51-03.local>
2018-11-21 13:02:41 -0500

    fifth commit made by rocio

---
+++ meet&travel/Model/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  meet&travel
+//
+//  Created by Developer User on 11/21/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var id: Int?
+    var firstname: String?
+    var lastname: String?
+    var email: String?
+    var telephone: String?
+    var dni: String?
+    var birthdate: String?
+    var password: String?
+    var confirmPassword: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstname
+        case lastname
+        case email
+        case telephone
+        case dni
+        case birthdate
+        case password
+        case confirmPassword = "confirm_password"
+    }
+}
